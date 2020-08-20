987f0a72603f557a54d4e31281de3c0a3ae48854
Atria <atriasft@gmail.com>
2020-08-21 02:34:18 +0900

    issue #7. improve QuickAccess.

--- TwD3/IconClickEvent.js
+++ TwD3/IconClickEvent.js
@@ -1,3 +1,26 @@
-chrome.browserAction.onClicked.addListener(function () {
-  chrome.tabs.create({url: "https://tweetdeck.twitter.com/" });
+chrome.browserAction.onClicked.addListener(function() {
+    chrome.tabs.query({}, tabs => {
+        for (let i = 0; i < tabs.length; i++) {
+            if (tabs[i].url === 'https://tweetdeck.twitter.com/') {
+                chrome.tabs.update(tabs[i].id, { active: true });
+
+                //通知
+                const options = {
+                    iconUrl: 'icon.png',
+                    type: 'list',
+                    title: "TwD3",
+                    message: '',
+                    priority: 1,
+                    items: [{
+                        title: 'detected',
+                        message: chrome.i18n.getMessage('MultipleTweetDeckWindow')
+                    }]
+                };
+                let notificationId = "notification";
+                chrome.notifications.create(notificationId, options);
+                return;
+            }
+        }
+        chrome.tabs.create({ url: "https://tweetdeck.twitter.com/" });
+    });
 });
